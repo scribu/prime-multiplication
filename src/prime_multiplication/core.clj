3d1f79d7bcd8a5dd0289d10e29ae38bf98c0af74
scribu <mail@scribu.net>
2017-10-16 16:56:14 +0300

    naive implementation of gen-primes

--- src/prime_multiplication/core.clj
+++ src/prime_multiplication/core.clj
@@ -1,10 +1,22 @@
 (ns prime-multiplication.core
   (:gen-class))
 
+(defn divisible? [a b]
+  (zero? (mod a b)))
+
+(defn prime? [n]
+  (and (> n 1) (not-any? (partial divisible? n) (range 2 n))))
+
 (defn gen-primes
   "Returns a list of the first n prime numbers."
   [n]
-  [])
+  (letfn [(compute [acc x]
+            (if (= (count acc) n)
+              acc
+              (if (prime? x)
+                (compute (conj acc x) (inc x))
+                (compute acc (inc x)))))]
+    (compute [] 2)))
 
 (defn -main
   [in]
--- test/prime_multiplication/core_test.clj
+++ test/prime_multiplication/core_test.clj
@@ -2,6 +2,13 @@
   (:require [clojure.test :refer :all]
             [prime-multiplication.core :refer :all]))
 
+(deftest test-prime?
+  (is (= (prime? 0) false))
+  (is (= (prime? 1) false))
+  (is (= (prime? 2) true))
+  (is (= (prime? 3) true))
+  (is (= (prime? 4) false)))
+
 (deftest test-gen-primes
   (is (= (gen-primes 0) []))
   (is (= (gen-primes 1) [2]))
