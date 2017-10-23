c696b3c4e0b8118829955e164cfb95ad564339b5
scribu <mail@scribu.net>
2017-10-23 22:15:40 +0300

    expose prime sieve as a lazy sequence

--- test/prime_multiplication/core_test.clj
+++ test/prime_multiplication/core_test.clj
@@ -2,15 +2,13 @@
   (:require [clojure.test :refer :all]
             [prime-multiplication.core :refer :all]))
 
-(deftest test-psieve
+(deftest test-prime-sieve-plumbing
   (is (= (-> (psieve-init) (psieve-current)) 2))
   (is (= (-> (psieve-init) (psieve-next) (psieve-next) (psieve-current)) 5)))
 
-(deftest test-gen-primes
-  (is (= (gen-primes 0) []))
-  (is (= (gen-primes 1) [2]))
-  (is (= (gen-primes 10) [2 3 5 7 11 13 17 19 23 29]))
-  (is (= (count (gen-primes 10000)) 10000)))
+(deftest test-prime-sieve
+  (is (= (take 10 (prime-sieve)) [2 3 5 7 11 13 17 19 23 29]))
+  (is (= (count (take 10000 (prime-sieve))) 10000)))
 
 (deftest test-multiplication-table
   (is (= (multiplication-table []) []))
